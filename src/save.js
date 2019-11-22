90a060ba2ac197cbc192868ceaad05fe4fe4a780
Ralf Hortt <me@horttcore.de>
2019-11-22 09:16:55 +0100

    Update save.js

--- src/save.js
+++ src/save.js
@@ -1,1 +1,31 @@
+const { __ } = wp.i18n;
+const { RichText } = wp.editor;
+const { InnerBlocks } = wp.editor;
 
+export default class Save extends Component {
+  constructor(props) {
+    super(...arguments);
+  }
+
+  render() {
+    const {
+      attributes: { title },
+      className
+    } = this.props;
+
+    return (
+      <div className="accordion">
+        <div className="accordion__content">
+          <RichText.Content
+            tagName="div"
+            className="accordion__title"
+            value={title}
+          />
+          <div className="accordion__body">
+           <InnerBlocks.Content />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
