6e90e20ddb8d2b20a48c7b354f7b87d2e36d3967
Ralf Hortt <me@horttcore.de>
2019-11-22 09:12:11 +0100

    Update edit.js

--- src/edit.js
+++ src/edit.js
@@ -1,1 +1,42 @@
+const { __ } = wp.i18n;
+const { RichText } = wp.editor;
+const { InnerBlocks } = wp.editor;
 
+export default class Edit extends Component {
+  constructor(props) {
+    super(...arguments);
+  }
+
+  render() {
+    const {
+      attributes: { title },
+      setAttributes,
+      className
+    } = this.props;
+
+    let classNames = `${className} accordion`;
+
+    return (
+      <div className={classNames}>
+        <div className="accordion__content">
+          <RichText
+            tagName="div"
+            className="accordion__title"
+            label={__("Title")}
+            value={title}
+            placeholder={__("Title")}
+            keepPlaceholderOnFocus
+            onChange={title => setAttributes({ title })}
+          />
+          <div className="accordion__body">
+            {"undefined" !== typeof props.insertBlocksAfter ? (
+              <InnerBlocks />
+            ) : (
+              ""
+            )}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
